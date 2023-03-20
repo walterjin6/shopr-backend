06594eeba557a81e5714323a38c4d1005e217752
WalterJin2 <walterjin2@gmail.com>
2023-03-20 21:10:08 +1100

    a

--- config/db.ts
+++ config/db.ts
@@ -7,7 +7,7 @@ export const connectDB = async () => {
   try {
     // creating a connection to the database
     const connection = await mongoose.connect(
-      'mongodb+srv://qqfhzk:Good01bye@cluster0.cuhdzgy.mongodb.net/ecommerce2023?retryWrites=true&w=majority'
+      'mongodb+srv://qqfhzk:Good01bye@cluster0.cuhdzgy.mongodb.net/proshopMern2022?retryWrites=true&w=majority'
     )
 
     console.log(`MongoDB connected : ${connection.connection.host}`);
